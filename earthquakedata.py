5bbaeb996fc45f92ed4956805ea3046bcbca97ce
marty1313170 <martin.han1@education.nsw.gov.au>
2026-03-16 08:19:44 +1100

    Filtering feature properly implemented
    Refactored and properly implemented code for filitering earthquake data. As well as instructions for users to use the interface

--- earthquakedata.py
+++ earthquakedata.py
@@ -23,24 +23,30 @@ def coords_to_location(place, closest=5):
         return None
 
     
-    lat = float(data[0]["lat"])
+    lat = float(data[0]["lat"]) # Finds Lat and Lon that USGS earthquake can only find via these two terms
     lon = float(data[0]["lon"])  
     print(f"Lat: {lat}, Lon: {lon}")
     return lat, lon
 
 
 
-def real_location(lat, lon, date, magnitude):
+def real_location(lat, lon, date, magnitude, start, end):
 
-    if magnitude == "":
+    if magnitude == "": # Decides if magntiude is filled in by the user, and if it isn't display 10 of the newest earthquake
        decide = 10
     else:
        decide = 1
 
+    params={"latitude": lat, "longitude": lon,  "format": "geojson" , "limit" : decide, "maxradiuskm" : 500}
+    if start:
+        params["starttime"] = start
+        params["endtime"] = end
+
 
     response_2 = requests.get(
         BASE_URL,
-        params={"latitude": lat, "longitude": lon,  "format": "geojson" , "limit" : decide, "maxradiuskm" : 500,}
+        params=params
+        
     )
    
     data_2 = response_2.json()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkcalendar import DateEntry
 import earthquakedata
 from tkinter import messagebox
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 root = tk.Tk()
 root.title("Earthquake lookup")
@@ -19,26 +19,32 @@ def call_data_search(closest_result = 0.3, time_closest = 86400000):
     magnitude = entry_3.get()
 
     if not location_area:
-        messagebox.showwarning("Missing information, will not continue")
+        messagebox.showwarning("Missing information, will not continue") # If even location is missing, will not continue
         return
-    if magnitude and date_earth == "":
+    if magnitude and date_earth == "": # If magnitude and date_earth is missing still continue
         earthquakedata.coords_to_location(location_area)
         return 
 
-
-    
     coords = earthquakedata.coords_to_location(location_area)
     if coords is None:
-        messagebox.showerror("Error")
+        messagebox.showerror("Error", "Could not find location try again with a vaild input ") # Failed to procceed if location is not found
         return
     
-    lat, lon = coords
-    result, is_fallback = earthquakedata.real_location(lat, lon, date_earth, magnitude)
-
     if date_earth:
        date_ms = datetime.strptime(date_earth, "%d-%m-%Y").replace(tzinfo=timezone.utc).timestamp() * 1000
+       start = datetime.strptime(date_earth, "%d-%m-%Y").strftime("%Y-%m-%d")
+       end = (datetime.strptime(date_earth, "%d-%m-%Y") + timedelta(days=1)).strftime("%Y-%m-%d")
     else:
        date_ms = None
+       start = None
+       end = None
+
+       
+
+    lat, lon = coords 
+    result, is_fallback = earthquakedata.real_location(lat, lon, date_earth, magnitude, start, end)
+
+    
 
     
     
@@ -46,61 +52,66 @@ def call_data_search(closest_result = 0.3, time_closest = 86400000):
 
     features = result["features"]
     exact_area = any(location_area.lower() in ed ["properties"]["place"].lower() for ed in features)
-    if not exact_area:
-        listbox.insert(0, "Could not find earthquake in exact location here are the closest ones!")
+    found_match = False
     for ed in features:
         place =  ed["properties"]["place"]
         mag = ed["properties"]["mag"]
         time = ed["properties"]["time"]
         if date_ms and abs(time - date_ms) < time_closest:
-           listbox.insert(tk.END, f"M{mag} - {place} (matched result)")
+           found_match = True
+           listbox.insert(tk.END, f"M{mag} - {place} (matched result)") # It found an exact location 
         elif magnitude and date_earth == "":
              print(result)  
-             listbox.insert(tk.END, f"M{mag} - {place} (newest result)")
+             listbox.insert(tk.END, f"M{mag} - {place} (newest result)") #Newest earthquake
         elif magnitude and abs (float(magnitude) - float (mag)) < closest_result:
              listbox.insert(tk.END, f"M{mag} - {place} (newest result)")
         elif magnitude and date_earth == "":
-            print(result)
+            print(result) 
         else:
             listbox.insert(tk.END, f"M{mag} - {place}")
-        
-        
+
 
     
+    if not exact_area and not found_match:
+        listbox.insert(0, "Could not find earthquake in exact location ") # If fails to find that specific of a earthquake then just finds a earthquake 500km within input location
+        listbox.insert(1, "here are the closest ones")
+        
     print(result)
 
 
 
 
 # Below are Visual Widgets 
+label_0 = tk.Label(root, text="To search earthquakes on this directory, fill out the location, other inputs such as \n the date and magnitude are optional but recommended for more accurate searches. Leave Date with NO characters if unknown", font=("helvetica", 12))
+label_0.place(x=600, y=50)
 
 label_1 = tk.Label(root, text="Welcome to the EarthQUAKE Directory", font=("impact", 32))
-label_1.place(x=440, y=0)
+label_1.place(x=590, y=-10)
 
 
 label_2 = tk.Label(root, text="Location of the earthquake?:" ,font=("Arial", 14))
-label_2.place(x=540, y=50)
+label_2.place(x=750, y=90)
 
 entry_1 = tk.Entry(root, width=30, bg=("blue"))
-entry_1.place(x=540, y=80)
+entry_1.place(x=750, y=120)
 
 button_1 = tk.Button(root, text="Search for this earthquake",  command=call_data_search)
-button_1.place(x=585, y=400)
+button_1.place(x=750, y=540)
 
 label_2 = tk.Label(root, text="Date of the earthquake" ,font=("Arial", 14))
-label_2.place(x=540, y=110)
+label_2.place(x=750, y=150)
 
 entry_2 = DateEntry(root, date_pattern='dd-mm-yyyy')
-entry_2.place(x=540, y=140)
+entry_2.place(x=750, y=180)
 
 label_3 = tk.Label(root, text="Magnitude of the earthquake? Up to 1 decimal point")
-label_3.place(x=540, y=180)
+label_3.place(x=750, y=220)
 
 entry_3 = tk.Entry(root, width=30, bg=("blue"))
-entry_3.place(x=540, y=220)
+entry_3.place(x=750, y=260)
 
 listbox = tk.Listbox(root, width=50,height=10)
-listbox.place(x=50, y=20)
+listbox.place(x=750, y=340)
 
 
 
